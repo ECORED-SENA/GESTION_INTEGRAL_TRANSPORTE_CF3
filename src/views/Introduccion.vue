ddce90a9a168b10211192e30435df95237605f77
luisEfe80 <luismendez80@hotmail.com>
2021-07-03 19:42:11 -0400

    content adjustment

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,20 +8,14 @@
         span
           i.fas.fa-info
       h1 Introducción
-    .SliderIntro.mb-5
-      SlyderA
-        div
-          figure
-            img(src='@/assets/curso/t0-i1.svg', alt='Texto que describa la imagen')
-        div
-          figure
-            img(src='@/assets/curso/t0-i2.svg', alt='Texto que describa la imagen')
-        div
-          figure
-            img(src='@/assets/curso/t0-i3.svg', alt='Texto que describa la imagen')
-      
-
     
+    .slider-wrapper.mb-5
+      p.item-1
+        | Bienvenidos al componente formativo documentación del transporte
+      p.item-2
+        | Este componente facilitará al aprendiz el conocimiento de la información necesaria respecto a los requisitos y documentos que facilitan el traslado de personas y/o productos de un lugar a otro.
+      p.item-3
+        | Con el cumplimiento de lineamientos técnicos y legales apropiados para cada sector del transporte.   
 
     p.mb-3  <b>El desarrollo del transporte está formado por diferentes componentes técnicos y legales</b> que permiten el traslado de personas y/o productos de un lugar a otro. Entre dichos componentes hace parte importante e imprescindible la documentación requerida que permita el movimiento vehicular, de la mercancía, de las personas e incluso que formalice la relación contractual entre los actores de la cadena del transporte.
     p.mb-3  En el presente componente formativo se busca desarrollar conocimientos en el aprendiz que faciliten el transporte mediante la interacción con los documentos técnicos y legales, utilizados en el desarrollo de las operaciones de transporte, para lo cual, se realizará una distinción necesaria por modo y medio, tanto a nivel nacional como internacional.   
@@ -54,7 +48,28 @@
 export default {
   name: 'Introduccion',
   data: () => ({
-    // variables de vue
+    datosSlyder: [
+      {
+        titulo:
+          'Bienvenidos al componente formativo documentación del transporte',
+        texto: '',
+        imagen: require('@/assets/curso/t0-i1.svg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo:
+          'Este componente facilitará al aprendiz el conocimiento de la información necesaria respecto a los requisitos y documentos que facilitan el traslado de personas y/o productos de un lugar a otro.',
+        texto: '',
+        imagen: require('@/assets/curso/t0-i1.svg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo:
+          'Con el cumplimiento de lineamientos técnicos y legales apropiados para cada sector del transporte.',
+        texto: '',
+        imagen: require('@/assets/curso/t0-i1.svg'),
+      },
+    ],
   }),
 }
 </script>
