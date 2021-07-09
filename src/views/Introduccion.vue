802e65d1eb544f0bae8ff2bfec83a8b652cfc2cf
luisEfe80 <luismendez80@hotmail.com>
2021-07-08 21:25:20 -0400

    Ajuste de contenido

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,13 +9,7 @@
           i.fas.fa-info
       h1 Introducción
     
-    .slider-wrapper.mb-5
-      p.item-1
-        | Bienvenidos al componente formativo documentación del transporte
-      p.item-2
-        | Este componente facilitará al aprendiz el conocimiento de la información necesaria respecto a los requisitos y documentos que facilitan el traslado de personas y/o productos de un lugar a otro.
-      p.item-3
-        | Con el cumplimiento de lineamientos técnicos y legales apropiados para cada sector del transporte.   
+    SlyderD.mb-5(:datos="datosSlyder")
 
     p.mb-3  <b>El desarrollo del transporte está formado por diferentes componentes técnicos y legales</b> que permiten el traslado de personas y/o productos de un lugar a otro. Entre dichos componentes hace parte importante e imprescindible la documentación requerida que permita el movimiento vehicular, de la mercancía, de las personas e incluso que formalice la relación contractual entre los actores de la cadena del transporte.
     p.mb-3  En el presente componente formativo se busca desarrollar conocimientos en el aprendiz que faciliten el transporte mediante la interacción con los documentos técnicos y legales, utilizados en el desarrollo de las operaciones de transporte, para lo cual, se realizará una distinción necesaria por modo y medio, tanto a nivel nacional como internacional.   
@@ -50,21 +44,21 @@ export default {
     datosSlyder: [
       {
         titulo:
-          'Bienvenidos al componente formativo documentación del transporte',
+          'Bienvenidos al componente formativo documentación del transporte.',
         texto: '',
         imagen: require('@/assets/curso/t0-i1.svg'),
         // leyendaImagen: 'Leyenda de la imagen',
       },
       {
         titulo:
-          'Este componente facilitará al aprendiz el conocimiento de la información necesaria respecto a los requisitos y documentos que facilitan el traslado de personas y/o productos de un lugar a otro.',
+          'Este componente facilitará al aprendiz el conocimiento de la información necesaria respecto a los requisitos y documentos que facilitan el traslado de personas y/o productos de un lugar a otro,',
         texto: '',
         imagen: require('@/assets/curso/t0-i1.svg'),
         // leyendaImagen: 'Leyenda de la imagen',
       },
       {
         titulo:
-          'Con el cumplimiento de lineamientos técnicos y legales apropiados para cada sector del transporte.',
+          'con el cumplimiento de lineamientos técnicos y legales apropiados para cada sector del transporte.',
         texto: '',
         imagen: require('@/assets/curso/t0-i1.svg'),
       },
